9d6e22ebdf5565a8ddf2791a8f806b3135b6e376
moshcat <mauricecathoverile@gmail.com>
2024-11-08 23:26:49 +0800

    Done repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (kata, jumlah) {
+  if (jumlah < 0) {
+    return "ERROR";
+  }
+  let string = "";
+  for (let i = 0; i < jumlah; i++) {
+    string += kata;
+  }
+  return string;
 };
 
 // Do not edit below this line
